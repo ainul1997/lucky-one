00ec6999010a1b67e564c3c151da56a644bc7e78
ainul1997 <ainul4147@gmail.com>
2022-03-26 22:56:20 +0600

    add react work

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header/Header';
 import Shop from './component/Shop/Shop';
+import Question from './component/Question/Question.js/Question';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Shop></Shop>
+      <Question></Question>
     </div>
   );
 }
